80e5004e6370d3ad96390c3c1f112cd754845f98
Maria Fructuoso <marifructuosog718@gmail.com>
2026-01-07 09:53:48 +0100

    3_1

---
+++ src/Controller/ImagenController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Imagen;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ImagenController extends AbstractController
+{
+    #[Route('/imagen', name: 'app_imagen')]
+    public function index(ManagerRegistry $doctrine): Response
+    {
+        $imagenes = $doctrine->getRepository(Imagen::class)->findAll();
+        return $this->render('imagen/index.html.twig', [
+            'imagenes' => $imagenes
+        ]);
+    }
+}
