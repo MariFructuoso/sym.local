2f3306e39bdd8979be2c901a28e53f4316dd54e7
Maria Fructuoso <marifructuosog718@gmail.com>
2026-01-22 20:16:32 +0100

    Parte 6_1 terminado hasta el punto 5 con ERRORES

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -8,6 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
+    #[Route('/', name: 'sym_index')]
     public function index()
     {
         $nombre = 'María';
