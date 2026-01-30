c0b7cb4c26003d934cd2b84a212a8d860df5aac7
Maria Fructuoso <marifructuosog718@gmail.com>
2026-01-30 10:39:02 +0100

    PARTE 7_1 TERMINADO CON NOTFOUND

---
+++ src/BLL/BaseBLL.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\BLL;
+
+use App\Entity\Imagen;
+use App\Repository\ImagenRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+
+abstract class BaseBLL
+{
+    protected EntityManagerInterface $em;
+    protected ValidatorInterface $validator;
+    protected RequestStack $requestStack;
+    protected Security $security;
+    protected ImagenRepository $imagenRepository;
+    public function __construct(
+        EntityManagerInterface $em,
+        ValidatorInterface $validator,
+        RequestStack $requestStack,
+        Security $security,
+        ImagenRepository $imagenRepository
+    ) {
+        $this->em = $em;
+        $this->validator = $validator;
+        $this->requestStack = $requestStack;
+        $this->security = $security;
+        $this->imagenRepository = $imagenRepository;
+    }
+
+
+    public function toArray(Imagen $imagen)
+    {
+        if (is_null($imagen))
+            return null;
+        return [
+            'id' => $imagen->getId(),
+            'nombre' => $imagen->getNombre(),
+            'descripcion' => $imagen->getDescripcion(),
+            'categoria' => $imagen->getCategoria()->getNombre(),
+            'numLikes' => $imagen->getNumLikes(),
+            'numVisualizaciones' => $imagen->getNumVisualizaciones(),
+            'numDownloads' => $imagen->getNumDownloads(),
+            'fecha' => is_null($imagen->getFecha()) ? '' : $imagen->getFecha()->format('d/m/Y'),
+            'usuario' => $imagen->getUsuario()->getId()
+        ];
+    }
+
+    private function validate($entity)
+    {
+        $errors = $this->validator->validate($entity);
+        if (count($errors) > 0) {
+            $strError = '';
+            foreach ($errors as $error) {
+                if (!empty($strError))
+                    $strError .= '\n';
+                $strError .= $error->getMessage();
+            }
+            throw new BadRequestHttpException($strError);
+        }
+    }
+
+    protected function guardaValidando($entity): array
+    {
+        $this->validate($entity); // Validación de los datos
+        $this->em->persist($entity); // Se guardan los datos
+        $this->em->flush();
+        return $this->toArray($entity); // Devolvemos la entidad en forma de array
+    }
+}
--- src/BLL/ImagenBLL.php
+++ src/BLL/ImagenBLL.php
@@ -2,35 +2,32 @@
 
 namespace App\BLL;
 
+
+use DateTime;
+use App\BLL\BaseBLL;
+use App\Entity\User;
+use App\Entity\Imagen;
+use App\Entity\Categoria;
 use Symfony\Component\HttpFoundation\RequestStack;
-use App\Repository\ImagenRepository;
 use Symfony\Bundle\SecurityBundle\Security; // <--- 1. NUEVO: Importar Security
 
-class ImagenBLL
+class ImagenBLL extends BaseBLL
 {
-    private RequestStack $requestStack;
-    private ImagenRepository $imagenRepository;
-    private Security $security; // <--- 2. NUEVO: Propiedad para seguridad
-
-    // 3. NUEVO: Inyectamos Security en el constructor
-    public function __construct(
-        RequestStack $requestStack, 
-        ImagenRepository $imagenRepository,
-        Security $security
-    )
+    public function setRequestStack(RequestStack $requestStack)
     {
         $this->requestStack = $requestStack;
-        $this->imagenRepository = $imagenRepository;
+    }
+    public function setSecurity(Security $security)
+    {
         $this->security = $security;
     }
-
     public function getImagenesConOrdenacion(?string $ordenacion)
     {
         if (!is_null($ordenacion)) { // Cuando se establece un tipo de ordenación específico
             $tipoOrdenacion = 'asc'; // Por defecto si no se había guardado antes en la variable de sesión
             $session = $this->requestStack->getSession(); // Abrir la sesión
             $imagenesOrdenacion = $session->get('imagenesOrdenacion');
-            
+
             if (!is_null($imagenesOrdenacion)) { // Comprobamos si ya se había establecido un orden
                 if ($imagenesOrdenacion['ordenacion'] === $ordenacion) // Por si se ha cambiado de campo a ordenar
                 {
@@ -52,9 +49,26 @@ class ImagenBLL
 
         // 5. NUEVO: Se lo pasamos al repositorio como tercer argumento
         return $this->imagenRepository->findImagenesConCategoria(
-            $ordenacion, 
-            $tipoOrdenacion, 
+            $ordenacion,
+            $tipoOrdenacion,
             $usuarioLogueado
         );
+    }
+    public function nueva(array $data)
+    {
+        $imagen = new Imagen();
+        $imagen->setNombre($data['nombre']);
+        $imagen->setDescripcion($data['descripcion']);
+        $imagen->setNumVisualizaciones($data['numVisualizaciones']);
+        $imagen->setNumLikes($data['numLikes']);
+        $imagen->setNumDownloads($data['numDownloads']);
+        // El id de la categoria, la tenemos que busar en su BBDD
+        $categoria = $this->em->getRepository(Categoria::class)->find($data['categoria']);
+        $imagen->setCategoria($categoria);
+        $fecha = DateTime::createFromFormat('d/m/Y', $data['fecha']);
+        $imagen->setFecha($fecha);
+        $usuario = $this->em->getRepository(User::class)->find($data['usuario']);
+        $imagen->setUsuario($usuario);
+        return $this->guardaValidando($imagen);
     }
 }
